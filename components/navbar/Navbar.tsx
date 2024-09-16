9466453aaade09271b329ac8ac0e4e4ffa72f2be
Natalia <natalia.calt@gmail.com>
2024-09-16 13:28:53 -0600

    Navbar Structure added

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -1,7 +1,19 @@
+import NavSearch from "./NavSearch";
+import LinksDropdown from "./LinksDropdown";
+import DarkMode from "./DarkMode";
+import Logo from "./Logo";
+
 export default function Navbar() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Navbar Page</h1>
-    </div>
+    <nav className="border-b">
+      <div className="container flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+        <Logo />
+        <NavSearch />
+        <div className="flex gap-4 items-center ">
+          <DarkMode />
+          <LinksDropdown />
+        </div>
+      </div>
+    </nav>
   );
 }
