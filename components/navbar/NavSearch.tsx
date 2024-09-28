2508fd71e47809e240eeca2a5c9667d56daed870
Natalia <natalia.calt@gmail.com>
2024-09-27 21:37:44 -0600

    Search functionality

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,11 +1,45 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
 import { Input } from "../ui/input";
 
 export default function NavSearch() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  const [search, setSearch] = useState(
+    searchParams.get("search")?.toString() || ""
+  );
+
+  const handleSearch = useDebouncedCallback((value: string) => {
+    const params = new URLSearchParams(searchParams);
+    if (value) {
+      params.set("search", value);
+    } else {
+      params.delete("search");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
+  useEffect(() => {
+    if (!searchParams.get("search")) {
+      setSearch("");
+    }
+  }, [searchParams.get("search")]);
+
   return (
     <Input
       type="text"
       placeholder="find a property..."
       className="max-w-xs dark:bg-muted"
+      value={search}
+      onChange={(e) => {
+        setSearch(e.target.value);
+        handleSearch(e.target.value);
+      }}
     />
   );
 }
