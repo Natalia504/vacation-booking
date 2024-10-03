19f098b6a188ef467e2ba732ae0b02feb85f37ca
Natalia <natalia.calt@gmail.com>
2024-10-03 14:16:41 -0600

    Fix build

--- components/card/PropertyCard.tsx
+++ components/card/PropertyCard.tsx
@@ -29,7 +29,9 @@ export default function PropertyCard({
           <h3 className="text-sm font-semibold mt-1">
             {name.substring(0, 30)}
           </h3>
-          <PropertyRating inPage={false} propertyId={propertyId} />
+          <PropertyRating
+            inPage={false} //propertyId={propertyId}
+          />
         </div>
         <p className="text-sm mt-1 text-muted-foreground ">
           {tagline.substring(0, 40)}
