129e3ca2ab24cca5d3962192fbf577a4db7bf978
Natalia <natalia.calt@gmail.com>
2024-10-08 21:33:49 -0600

    ReviewInput component is set

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -13,6 +13,7 @@ import { Description } from "@/components/properties/Description";
 import Amenities from "@/components/properties/Amenities";
 import dynamic from "next/dynamic";
 import { Skeleton } from "@/components/ui/skeleton";
+import SubmitReview from "@/components/reviews/SubmitReview";
 
 const DynamicMap = dynamic(
   () => import("@/components/properties/PropertyMap"),
@@ -66,6 +67,7 @@ export default async function PropertyDetailsPage({
           <BookingCalendar />
         </div>
       </section>
+      <SubmitReview propertyId={property.id} />
     </section>
   );
 }
---
+++ components/reviews/Rating.tsx
@@ -0,0 +1,3 @@
+export default function PropertyReviews() {
+  return <div>PropertyReviews</div>;
+}
